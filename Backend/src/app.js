2e53617917b69d86ad6204e26ddf0d817640167e
Sarthakagrawal213 <sarthakagrawal213@gmail.com>
2026-03-05 08:51:57 +0530

    Fix CORS and cookie settings

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:5173', // Frontend URL
+  origin: 'https://aimusicplayer-1.onrender.com', // Frontend URL
   credentials: true, // Allow cookies to be sent
 }));
 const authRoutes = require('./routes/auth.routes');
